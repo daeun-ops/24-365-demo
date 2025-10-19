b1e3b9ea47691f4636c740fc48e3ebc7b0f23b9e
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-19 13:21:50 +0900

    Update vpc-endpoints.tf

--- infra/terraform/vpc-endpoints.tf
+++ infra/terraform/vpc-endpoints.tf
@@ -1,4 +1,3 @@
-# 필수 엔드포인트(프라이빗 패스): S3/DynamoDB(게이트웨이) + ECR/STS/KMS/Logs(인터페이스)
 data "aws_vpc" "this" { id = module.vpc.vpc_id }
 
 resource "aws_vpc_endpoint" "s3" {
@@ -17,16 +16,10 @@ resource "aws_vpc_endpoint" "dynamodb" {
   tags = { Name = "${var.name}-vpce-dynamodb" }
 }
 
-locals {
-  interface_subnet_ids = module.vpc.private_subnets
-  sg_id                = aws_security_group.vpce_sg.id
-}
-
 resource "aws_security_group" "vpce_sg" {
   name        = "${var.name}-vpce-sg"
-  description = "VPC endpoints SG"
   vpc_id      = module.vpc.vpc_id
-  egress { from_port = 0 to_port = 0 protocol = "-1" cidr_blocks = ["0.0.0.0/0"] }
+  egress  { from_port = 0 to_port = 0 protocol = "-1" cidr_blocks = ["0.0.0.0/0"] }
   ingress { from_port = 443 to_port = 443 protocol = "tcp" cidr_blocks = [module.vpc.vpc_cidr_block] }
 }
 
@@ -45,8 +38,8 @@ resource "aws_vpc_endpoint" "interfaces" {
   vpc_id              = module.vpc.vpc_id
   service_name        = each.value
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = local.interface_subnet_ids
-  security_group_ids  = [local.sg_id]
+  subnet_ids          = module.vpc.private_subnets
+  security_group_ids  = [aws_security_group.vpce_sg.id]
   private_dns_enabled = true
   tags = { Name = "${var.name}-vpce-${replace(each.value, "com.amazonaws.${var.region}.", "")}" }
 }
