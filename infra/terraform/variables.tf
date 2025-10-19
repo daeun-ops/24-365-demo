e0e060591b6ad3a7348979c2d9a8883566e4ec00
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-19 14:26:33 +0900

    Update variables.tf

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,1 +1,5 @@
-
+variable "region"           { type = string, default = "ap-northeast-2" }
+variable "name"             { type = string, default = "exchange-prod" }
+variable "eks_cluster_name" { type = string, default = "exchange-prod-eks" }
+variable "vpc_id"           { type = string, default = "vpc-123456" }
+variable "onprem_vpn_id"    { type = string, default = "vpn-1234abcd" }
