370b9dabe4bc6743835675b4c42d2e8a711e7c9f
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-19 13:23:11 +0900

    Update ssm-ec2.tf

--- infra/terraform/ssm-ec2.tf
+++ infra/terraform/ssm-ec2.tf
@@ -1,7 +1,4 @@
-# 무SSH 운영 ssm Session Manager 사용
-data "aws_iam_role" "karpenter_node_role" {
-  name = "KarpenterNodeRole-1234"
-}
+data "aws_iam_role" "karpenter_node_role" { name = "KarpenterNodeRole-1234" }
 
 resource "aws_iam_role_policy_attachment" "ssm_core" {
   role       = data.aws_iam_role.karpenter_node_role.name
